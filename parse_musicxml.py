e4412f7657f0531b4b17c3fdb42012ab7c34fb65
Ilana Shapiro <ilanashapiro@Ilanas-MacBook-Pro.local>
2020-10-30 14:57:51 -0700

    create dictionaries for raw and normalized output and probability transition matrices

--- parse_musicxml.py
+++ parse_musicxml.py
@@ -1,17 +1,21 @@
 import xml.etree.ElementTree as ET
+import collections
 
 class Parser:
     def __init__(self, filename):
         self.root = ET.parse(filename).getroot()
-        self.categories_dict = {}
-        self.chords_dict = {}
-        self.normalized_categories_dict = {}
-        self.initial_transition_matrix = {}
+        self.categories_dict = collections.OrderedDict()
+        self.normalized_categories_dict = collections.OrderedDict()
+        self.initial_transition_matrix = collections.OrderedDict()
+        self.normalized_initial_transition_matrix = collections.OrderedDict()
+        self.transition_probability_matrix = collections.OrderedDict()
+        self.normalized_transition_probability_matrix = collections.OrderedDict()
         self.smallest_note_value = None
         self.parse_categories()
 
     def parse_categories(self):
-        prev_note = None
+        prev_note = None # the prev note (it may be part of a chord). but this variable itself NEVER stores a chord
+        prev_sound_object = None # either note or chord
         prev_duration = None
         in_chord = False
         note = None
@@ -25,12 +29,18 @@ class Parser:
                     if int(duration) == 1 and not self.smallest_note_value and note_info.find('type') is not None:
                         self.smallest_note_value = self.switch_note_value(note_info.find('type').text)
 
+                    note = None
                     if note_info.find('pitch') is not None:
                         value = note_info.find('pitch').find('step').text if note_info.find('pitch').find('step') is not None else ''
                         octave = note_info.find('pitch').find('octave').text if note_info.find('pitch').find('octave') is not None else ''
                         accidental_info = note_info.find('accidental').text if note_info.find('accidental') is not None else None
                         accidental = '' if accidental_info is None else ('#' if accidental_info == 'sharp' else 'b' )
                         note = value + accidental + octave
+                    elif note_info.find('chord') is None: # means that: note_info.find('rest') is not None ----> so we are in a rest
+                        note = 'R'
+
+                    if note is not None:
+                        is_last_iteration = i == len(self.root.findall('part')) - 1 and j == len(part.findall('measure')) - 1 and k == len(measure.findall('note')) - 1
 
                         if note_info.find('chord') is not None:
                             if in_chord:
@@ -40,42 +50,67 @@ class Parser:
                             in_chord = True
                         elif in_chord and note_info.find('chord') is None:
                             in_chord = False
-                            self.insert(self.chords_dict, tuple(chord), prev_duration)
-                            self.insert(self.categories_dict, tuple(chord), prev_duration)
-                        elif i == len(self.root.findall('part')) - 1 and j == len(part.findall('measure')) - 1 and k == len(measure.findall('note')) - 1:
+                            curr_sound_object = tuple(chord)
+                            if prev_sound_object is not None:
+                                self.insert(self.transition_probability_matrix, prev_sound_object, curr_sound_object)
+                            prev_sound_object = curr_sound_object
+                            self.insert(self.categories_dict, curr_sound_object, prev_duration)
+                        elif is_last_iteration:
+                            self.insert(self.transition_probability_matrix, prev_sound_object, note)
                             self.insert(self.categories_dict, note, duration)
                         else:
+                            if prev_sound_object is not None:
+                                self.insert(self.transition_probability_matrix, prev_sound_object, note)
+                            prev_sound_object = note
                             self.insert(self.categories_dict, prev_note, duration)
-                    else: # means that: note_info.find('rest') != None
-                        note = 'R'
+
+                    if not in_chord:
+                        if note in self.initial_transition_matrix:
+                            self.initial_transition_matrix[note] = self.initial_transition_matrix[note] + 1
+                        else:
+                            self.initial_transition_matrix[note] = 1
 
                     prev_note = note
                     prev_duration = duration
-        for key in self.categories_dict:
-            print(key, self.categories_dict[key])
 
-    def insert(self, dict, type, duration):
-        if type in dict:
-            if duration in dict[type]:
-                dict[type][duration] = dict[type][duration] + 1
+    def insert(self, dict, value1, value2):
+        if value1 in dict:
+            if value2 in dict[value1]:
+                dict[value1][value2] = dict[value1][value2] + 1
             else:
-                dict[type][duration] = 1
+                dict[value1][value2] = 1
         else:
-            dict[type] = {}
-            dict[type][duration] = 1
+            dict[value1] = {}
+            dict[value1][value2] = 1
 
-    def normalize_categories(self):
-        for note in self.categories_dict:
-            self.normalized_categories_dict[note] = {}
+    def normalize_nested_matrix(self, input_matrix, output_matrix):
+        for type1 in input_matrix:
+            output_matrix[type1] = {}
             total_count = 0
-            for duration in self.categories_dict[note]:
-                total_count += self.categories_dict[note][duration]
-            for duration in self.categories_dict[note]:
-                self.normalized_categories_dict[note][duration] = self.categories_dict[note][duration] / total_count
-        # print(self.normalized_categories_dict)
+            for type2 in input_matrix[type1]:
+                total_count += input_matrix[type1][type2]
+            for type2 in input_matrix[type1]:
+                output_matrix[type1][type2] = input_matrix[type1][type2] / total_count
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        self.print_dict(input_matrix)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        self.print_dict(output_matrix)
 
-    def get_initial_transition_matrix(self):
-        self.initial_transition_matrix = {}
+    def normalize_initial_transition_matrix(self):
+        total_count = 0
+        for sound_object in self.initial_transition_matrix:
+            total_count += 1
+        for sound_object in self.initial_transition_matrix:
+            self.normalized_initial_transition_matrix[sound_object] = self.initial_transition_matrix[sound_object] / total_count
+
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        self.print_dict(self.initial_transition_matrix)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        self.print_dict(self.normalized_initial_transition_matrix)
+
+    def print_dict(self, dict):
+        for key in dict:
+            print(key, ":", dict[key])
 
     def switch_note_value(self, note_type):
         switcher = {
@@ -92,4 +127,5 @@ class Parser:
 
 if __name__ == "__main__":
     parser = Parser('Cantabile-Piano.musicxml')
-    parser.normalize_categories()
+    # parser.normalize_matrix(parser.categories_dict)
+    parser.normalize_initial_transition_matrix()
